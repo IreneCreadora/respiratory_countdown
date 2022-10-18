31bb09a74a92cd918f67780cf290f8fa53693261
IreneCreadora <nargiza.dancer@gmail.com>
2022-10-18 20:41:46 +0300

    Create christmasTasks.js

---
+++ src/js/christmasTasks.js
@@ -0,0 +1,127 @@
+export const christmasTasks = [
+  {
+    day: '1',
+    title: 'Title 1',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '2',
+    title: 'Title 2',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '3',
+    title: 'Title 3',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '4',
+    title: 'Title 4',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '5',
+    title: 'Title 5',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '6',
+    title: 'Title 6',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '7',
+    title: 'Title 7',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '8',
+    title: 'Title 8',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '9',
+    title: 'Title 9',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '10',
+    title: 'Title 10',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '11',
+    title: 'Title 11',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '12',
+    title: 'Title 12',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '13',
+    title: 'Title 13',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '14',
+    title: 'Title 14',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '15',
+    title: 'Title 15',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '16',
+    title: 'Title 16',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '17',
+    title: 'Title 17',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '18',
+    title: 'Title 18',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '19',
+    title: 'Title 19',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '20',
+    title: 'Title 20',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '21',
+    title: 'Title 21',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '22',
+    title: 'Title 22',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '23',
+    title: 'Title 23',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '24',
+    title: 'Title 24',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+  {
+    day: '25',
+    title: 'Title 25',
+    text: 'Lorehbkjmdt,n vcl,nh p;l,cxfdplv z;pld,sc;pzl,sdcv;pl,fdaplv,fel',
+  },
+];
