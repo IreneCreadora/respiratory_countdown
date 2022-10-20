9a81ef8aaeb1575b2767c7029a59ff15920f9b42
IreneCreadora <nargiza.dancer@gmail.com>
2022-10-20 19:49:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import { calendar } from './js/calendar';
 import { refs } from './js/refs';
+import { calendar } from './js/calendar';
+import { Timer } from './js/timer';
+import * as basicLightbox from 'basiclightbox';
+
 calendar();
+
+const newYearDate = new Date('2023-01-01 00:00:00');
+
+const timer = new Timer('.timer__items', newYearDate);
+
+refs.startTimer.addEventListener('click', timer.start.bind(timer));
