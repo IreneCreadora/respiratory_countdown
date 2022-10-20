b9a5951d94d7fa0446ffeb352338abff32525eb2
IreneCreadora <nargiza.dancer@gmail.com>
2022-10-20 19:49:42 +0300

    add new year timer

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -5,14 +5,12 @@ import * as basicLightbox from 'basiclightbox';
 
 export function calendar() {
   const markup = [...christmasTasks].map(createMarkup);
-  console.log(markup);
-  refs.calendar.innerHTML = markup.join('');
+  // console.log(markup);
+  refs.calendar.insertAdjacentHTML('beforeend', markup.join(''));
 }
 
-function createMarkup({ day, title, text }) {
-  return `<li class="calendar__item"><a><span>${day}</span>
-    <p class="calendar__title is-hidden">${title}</p>
-    <p class="calendar__text is-hidden">${text}</p>
+function createMarkup({ day }) {
+  return `<li class="calendar__item" data-day="${day}"><a href=""><span>${day}</span>
     </a>
     </li>`;
 }
@@ -20,20 +18,20 @@ function createMarkup({ day, title, text }) {
 refs.calendar.addEventListener('click', onCalendarClick);
 
 function onCalendarClick(e) {
-  if (e.target.nodeName !== 'LI') {
-    console.log(`click not li`);
+  if (e.target.nodeName === 'UL') {
     return;
   }
   e.preventDefault();
-  console.log(`${e}`);
-  modalShow();
+  const currentDay = e.target.dataset.day;
+  modalShow(currentDay);
 }
 
 let instance;
 
-function modalShow({ title, text }) {
+function modalShow(currentDay) {
+  const { title, text } = christmasTasks.find(task => task.day === currentDay);
   instance = basicLightbox.create(
-    `<div class="modal" width="800" height="600">
+    `<div class="modal">
         <p class="calendar__title">${title}</p>
         <p class="calendar__text">${text}</p>
     </div>`,
